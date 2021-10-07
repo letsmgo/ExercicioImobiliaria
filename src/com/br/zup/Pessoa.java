085ea5971ddb277068d157e529dd612da4af52e8
Let Marçal <leticiamarcalmatias@gmail.com>
2021-10-07 16:18:18 -0300

    Modelar classe pessoa

--- src/com/br/zup/Pessoa.java
+++ src/com/br/zup/Pessoa.java
@@ -1,5 +1,41 @@
 package com.br.zup;
 
 public class Pessoa {
+    //Atributos da superClasse Pessoa, que são comuns para Morador e Funcionario
+    private String nome;
+    private String cpf;
 
+    //Método construtor
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    //Getters and Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    //toString
+
+    @Override
+    public String toString() {
+        StringBuilder dados = new StringBuilder();
+        dados.append("Nome: " + nome);
+        dados.append("CPF: " + cpf);
+
+        return dados.toString();
+    }
 }
