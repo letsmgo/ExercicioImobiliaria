c13710987425e0b5c2fe8347c63a024f0c2fdf66
Let Marçal <leticiamarcalmatias@gmail.com>
2021-10-07 16:31:26 -0300

    Modelar classe Morador

--- src/com/br/zup/Morador.java
+++ src/com/br/zup/Morador.java
@@ -1,4 +1,42 @@
 package com.br.zup;
 
-public class Moradores {
+public class Morador extends Pessoa {
+    //Atributos específicos da classe Morador
+    private String telefone;
+    private double renda;
+
+    //Método construtor, contendo o super que é o construtor da superClasse
+    public Morador(String nome, String cpf, String telefone, double renda) {
+        super(nome, cpf);
+        this.telefone = telefone;
+        this.renda = renda;
+    }
+
+    //Getters and Setters
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public double getRenda() {
+        return renda;
+    }
+
+    public void setRenda(double renda) {
+        this.renda = renda;
+    }
+
+    //toString da classe contendo o super que pega os atributos da superclasse
+    @Override
+    public String toString() {
+        StringBuilder dados = new StringBuilder();
+        dados.append(super.toString());
+        dados.append("Número de contato: " + telefone);
+        dados.append("Renda mensal: R$" + renda);
+
+        return dados.toString();
+    }
 }
